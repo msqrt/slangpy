36d7183edd4b5d43e61076b54f099445fec3da82
Pauli Kemppinen <pauli.kemppinen@aalto.fi>
2026-06-11 13:41:51 +0300

    go back

--- slangpy/core/calldata.py
+++ slangpy/core/calldata.py
@@ -425,8 +425,6 @@ class CallData(NativeCallData):
         codegen = CodeGen()
         generate_code(context, build_info, bindings, codegen)
 
-        codegen.add_import(build_info.module.name)
-
         # TODO: This is not technically needed anymore because in the case of
         # linking multiple modules, we already have composed the module to begin with.
         for link in build_info.module.link:
